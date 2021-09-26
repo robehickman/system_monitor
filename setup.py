54fa22677e09e514ff16ea9dbef3df47065500a1
Robert Hickman <you@example.com>
2021-09-26 20:23:42 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='system monitor',
+    version='0.1',
+    description='Network enabled system monitor',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='Network system monitor',
+    author='Robert Hickman',
+    author_email='robehickman@gmail.com',
+    license='MIT',
+    packages=['system_monitor'],
+    install_requires=[
+        'pysdl2',
+        'pysdl2-dll',
+    ],
+    scripts=['monitor_server.py'],
+    zip_safe=False)
+
